46270a28f05f91ae1d0e160407883ab5ccf177c0
Stefan Garces <stefangarces@gmail.com>
2020-11-17 08:47:52 +0100

    First commit, just to connect it to github and the repo.

---
+++ Projektet_Csharp/Operators.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektet_Csharp
+{
+    class Operators
+    {
+    }
+}
