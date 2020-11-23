f8a4874dcc216cbf099d87b98fd8b335c8165d31
Stefan Garces <stefangarces@gmail.com>
2020-11-23 23:50:55 +0100

    Added a if statement that looks for "Plus, Minus and Times".

--- Projektet_Csharp/Program.cs
+++ Projektet_Csharp/Program.cs
@@ -10,6 +10,39 @@ namespace Projektet_Csharp
     {
         static void Main(string[] args)
         {
+
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(' ');
+
+            if (input.Contains("plus"))
+            {
+                Console.WriteLine("yes it does.");
+            }
+            else if (input.Contains("minus"))
+            {
+                Console.WriteLine("Yes, minus");
+            }
+            else if (input.Contains("times"))
+            {
+                Console.WriteLine("Yes, times brother");
+            }
+
+            Console.ReadLine();
+
+            /*
+            int num1;
+            int num2;
+            while (true)
+            {
+                Console.Write("Enter the First Number : ");
+                if (int.TryParse(Console.ReadLine(), out num2))
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter an integer value!");
+            }
+
+            /*
             string string1 = "1000";
             string string2 = "500";
 
@@ -20,6 +53,7 @@ namespace Projektet_Csharp
 
             Console.WriteLine(number1);
             Console.ReadLine();
+            */
         }
     }
 }
